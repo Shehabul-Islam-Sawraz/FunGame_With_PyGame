5b939d138a3473317ee9009880efe700ddc79fad
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2022-11-11 12:17:31 +0600

    Rotaion of ship added

--- Space-Invaders/Space_Invaders.py
+++ Space-Invaders/Space_Invaders.py
@@ -15,6 +15,17 @@ WIDTH = 800
 HEIGHT = 600      
 time = 0
 
+
+# Placing the ship in the center from top 
+# left (50 is used for centering the image as ship
+# image is 100x100. So 50x50 is the middle) 
+# at the start of the game
+ship_x = WIDTH/2 - 50
+ship_y = HEIGHT/2 - 50
+# This angle will help to rotate the ship. Inititally
+# the ship is faced to right. So angle=0
+ship_angle = 0
+
 # Canvas Declaration
 window = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32) # Creating the screen
 pygame.display.set_caption('Space Invaders') # Giving title to the screen
@@ -22,6 +33,18 @@ pygame.display.set_caption('Space Invaders') # Giving title to the screen
 # Load Images
 bg = pygame.image.load(os.path.join('images','bg.jpg'))
 debris = pygame.image.load(os.path.join('images','debris.png'))
+ship = pygame.image.load(os.path.join('images','ship.png'))
+
+# Function to rotate an image at specified angle
+def rot_center(image, angle):
+    """rotate a Surface, maintaining position."""
+
+    orig_rect = image.get_rect()
+    rot_image = pygame.transform.rotate(image, angle)
+    rot_rect = orig_rect.copy()
+    rot_rect.center = rot_image.get_rect().center
+    rot_image = rot_image.subsurface(rot_rect).copy()
+    return rot_image
 
 # Draw Game on Canvas
 def draw(canvas):
@@ -31,6 +54,7 @@ def draw(canvas):
     canvas.blit(debris,(time*0.3,0)) # Showing debris on top of background
     canvas.blit(debris,(time*0.3-WIDTH,0)) # This helps to create a moving effect of debris
     time = time + 1
+    canvas.blit(rot_center(ship,ship_angle), (ship_x, ship_y)) # Shows the ship at specified position & angle
     
 # Handle Input
 def handle_input():
